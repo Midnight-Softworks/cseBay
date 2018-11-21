30e4e5770323d3b3c562beec7b8d4637453a624b
Midnight-Softworks <sfs111@msstate.edu>
2018-11-21 13:31:16 -0600

    Included the header and ssp class

---
+++ Example Database Site/header.php
@@ -0,0 +1,17 @@
+<body>
+<div class="jumbotron" style="margin: 0; color: #351648; text-align:center">
+	<h1>Games Database</h1>
+</div>
+
+
+		<ul class="nav">
+			<li class="nav-item"><a class="nav-link" href="/" style="color:black">Home</a></li>
+			<li class="nav-item"><a class="nav-link" href="add_game.php" style="color:black">Add a game</a></li>
+		</ul>
+
+
+
+
+
+
+
